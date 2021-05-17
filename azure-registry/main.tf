edb4bbe45f22363c4e7bf682d5950b6e2cbb2e60
Rob Smith <rosmith@statestitle.com>
2021-05-16 20:28:59 -0700

    One more time

--- azure-registry/main.tf
+++ azure-registry/main.tf
@@ -1,10 +1,15 @@
 terraform {
   required_version = "= 0.15.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 2.59.0"
+    }
+  }
 }
 
 provider "azurerm" {
-  source  = "hashicorp/azurerm"
-  version = "= 2.59.0"
   features {}
 }
 
