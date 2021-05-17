a1f1c9914595f01db92f763c506bd1cb30b697f3
Rob Smith <rosmith@statestitle.com>
2021-05-16 20:27:18 -0700

    Reconfigured provider block

--- azure-registry/main.tf
+++ azure-registry/main.tf
@@ -1,13 +1,11 @@
 terraform {
   required_version = "= 0.15.3"
+}
 
-  required_providers {
-    azurerm {
-      source  = "hashicorp/azurerm"
-      version = "= 2.59.0"
-    }
-    features {}
-  }
+provider "azurerm" {
+  source  = "hashicorp/azurerm"
+  version = "= 2.59.0"
+  features {}
 }
 
 // Create Azure Container Registry
