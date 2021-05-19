9ea4d4a78154e3627f5b909ce8ddaa7f7d22dc4d
Rob Smith <rosmith@statestitle.com>
2021-05-19 13:40:25 -0700

    Added provider information

--- azure-resource-group/main.tf
+++ azure-resource-group/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = "= 0.15.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 2.59.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 data "azurerm_resource_group" "resource_group" {
   name = var.name
 }
